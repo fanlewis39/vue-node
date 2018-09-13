36c347a62fc962ea3d59448f2c78a9be498a63f5
fanlewis39 <756428834@qq.com>
2018-09-13 16:15:44 +0800

    vue

---
+++ vue/App.vue
@@ -0,0 +1,27 @@
+<!-- 模板：html结构 -->
+<template>
+  <div id="app">
+      <router-view></router-view>
+      <ul>
+        <li><router-link to="/">home</router-link></li>
+        <li><router-link to="/cart">购物车</router-link></li>
+      </ul>
+  </div>
+</template>
+
+<!-- 行为：处理逻辑 -->
+<script>
+
+//局部注册组件
+import Users from './components/Users'
+import Header from './components/Header'
+
+export default {
+  name: 'App'
+}
+</script>
+
+<!-- 样式：解决样式 -->
+<style scoped>
+    
+</style>
---
+++ vue/main.js
@@ -0,0 +1,33 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import Users from './components/Users'
+import VueRouter from 'vue-router'//引入路由模块
+import Cart from './components/Cart'
+import Home from './components/Home'
+import axios from 'axios'
+import VueAxios from 'vue-axios'//配置axios插件
+
+Vue.config.productionTip = false
+Vue.use(VueRouter)//注明使用
+Vue.use(VueAxios, axios)
+
+const router = new VueRouter({
+	routes:[
+        {path:"/",component:Home},
+        {path:"/cart",component:Cart}//cart是路由名字
+	],
+	mode:"history"//去掉井号
+})
+//全局注册组件
+// Vue.component("users",Users);
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+
